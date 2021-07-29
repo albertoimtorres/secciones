eb46a9711a38799ee8ac5a79242325e1f6381dca
Alberto Moreno <albertoimtorres@gmail.com>
2021-07-29 02:17:30 -0500

    Validation and payload

--- src/app/components/sections/sections.component.ts
+++ src/app/components/sections/sections.component.ts
@@ -19,12 +19,16 @@ export class SectionsComponent implements OnInit {
 
   isError: Boolean = false;
 
+  idSection?: number;
+
+  idCatalog?: number;
+
   /* Objects */
   sections?: Campo[];
 
   catalogs?: Catalogo[] = [];
 
-  payload?: number[] = [];
+  payload?: any = {};
 
   listSections: number [] = [
     1, 2, 3, 4, 5
@@ -67,17 +71,9 @@ export class SectionsComponent implements OnInit {
     /* Realiza la petición al servicio de sección y obtiene la data */
     this.currentSection = 1;
 
-    console.log('ESCENARIO 2');
+    this.payload = {};
 
     this.selectSection(this.currentSection);
-    // this.sectionService.getSection(this.currentSection).subscribe(
-    //   data => {
-    //     this.sections = data.secciones[0].campos;    
-    //   },
-    //   error => {
-    //     throw new Error(error);
-    //   }
-    // );
   }
 
   get field() {return this.form.controls;}
@@ -102,39 +98,50 @@ export class SectionsComponent implements OnInit {
     }
   }
 
-  async onCheckBoxChange(event: any) {
+  async onCheckBoxChange(event: any, obj: any) {
+    const {value, checked} = event.target;
+    const checkSection: FormArray = this.form.get('checkSection') as FormArray;
 
-    // const checkSection: FormArray = this.form.get('checkSection') as FormArray;
-    // console.log('CHECK SECTION: ', checkSection);
-    
-    console.log('EVENT TARGET VALUE: ', event.target.value);
-    
-    let idCampo = +event.target.value;
+    console.log('CHECKED: ', checked, 'VALUE: ', value, obj);
 
-    if (idCampo) {
+    if (checked && _.has(obj, 'catalogo')) {
+      this.payload = {idCampo: value, catalogs: []};
+      this.modal?.open(obj);
+    } else if (checked) {
+      if (_.has(obj, 'id_valor')) {
+        this.payload.catalogs.push(value);
 
-      let data = this.sections?.filter(section => _.isEqual(section.id_campo, idCampo))[0];
-
-      if (_.has(data, 'catalogo')) {
-
-        await this.modal?.open(data);
-
+        checkSection.push(new FormControl(this.payload));
       } else {
-        //! Arreglo de ids
-        this.payload?.push(idCampo);
+        checkSection.push(new FormControl({idCampo: value}));
       }
     } else {
-      console.log('FALSO: ', event.target.value);
+      let index = 0;
+
+      if (_.has(obj, 'id_valor')) {
+        console.log(checkSection, value);
+      }
+
+      checkSection.controls.forEach((item) => {
+        if (_.has(item.value, 'catalogs')) {
+          if (_.isEqual(item.value.idCampo, value)) {
+            checkSection.removeAt(index);
+            return;
+          } else if (_.includes(item.value.catalogs, value)) {
+            item.value.catalogs = _.filter(item.value.catalogs, catalog => catalog !== value);
+            return;
+          }
+        } else if (_.isEqual(item.value.idCampo, value)) {
+          checkSection.removeAt(index);
+          return;
+        }
+        index++;
+      });
     }
   }
 
   submitForm() {
-    if (_.size(this.payload) > 0) {
-      this.isError = false;
-      console.log('SEND PAYLOAD: ', this.payload);
-    } else {
-      this.isError = true;
-    }
+    console.log(_.uniqBy(this.form.value.checkSection, 'idCampo'));
   }
 
 }
--- src/app/interfaces/sections.interface.ts
+++ src/app/interfaces/sections.interface.ts
@@ -25,3 +25,51 @@ export interface Valore {
     descripcion:  string;
     indice_orden: number;
 }
+
+// export class Valores {
+    //     id_valor: string;
+    //     descripcion: string;
+    //     indice_orden: number;
+    
+    //     constructor(id_valor: string, description: string, indice_orden: number) {
+    //         this.id_valor = id_valor;
+    //         this.descripcion = description;
+    //         this.indice_orden = indice_orden;
+    //     }
+    // }
+    
+    // export class Catalogo {
+    //     id_catalogo: number;
+    //     valores: Valores[];
+    
+    //     constructor(id_catalogo: number, valores: Valores[]) {
+    //         this.id_catalogo = id_catalogo;
+    //         this.valores = valores;
+    //     }
+    // }
+    
+    // export class Campos {
+    //     id_campo: number;
+    //     etiqueta: string;
+    //     obligatorio: boolean;
+    //     catalogo: Catalogo;
+    
+    //     constructor(id_campo: number, etiqueta: string, obligatorio: boolean, catalogo: Catalogo) {
+    //         this.id_campo = id_campo;
+    //         this.etiqueta = etiqueta;
+    //         this.obligatorio = obligatorio;
+    //         this.catalogo = catalogo;
+    //     }
+    // }
+    
+    // export class Secciones {
+    //     id_seccion: number;
+    //     seccion: string;
+    //     campos: Campos[];
+    
+    //     constructor(id_seccion: number, seccion: string, campos: Campos[]) {
+    //         this.id_seccion = id_seccion;
+    //         this.seccion = seccion;
+    //         this.campos = campos;
+    //     }
+    // }
